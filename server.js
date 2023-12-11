d84b218cfe3e3e03648313dca0beffc0e46812f8
Olugbade Ayodeji <olugbade.ayodeji@gmail.com>
2023-12-11 16:49:40 +0100

    new commit

--- server.js
+++ server.js
@@ -4,6 +4,11 @@ const bodyParser = require('body-parser');
 const app = express();
 
 
+app.get('/', (req, res) => {
+    
+    res.send('Welcome')
+})
+
 app.listen(3000,() => {
     console.log('listening on port 3000')
 })
